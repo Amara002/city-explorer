dbb894c9d505243abcbfb4c9e23a85ff6f3c74b0
Amara002 <amaraalbalkhi@yahoo.com>
2021-06-04 15:48:11 +0300

    finish lab8

---
+++ src/movie.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+    Col,
+    Row,
+    Card,ListGroup,ListGroupItem
+} from 'react-bootstrap';
+
+class Movie extends React.Component {
+
+    render() {
+        return (
+            this.props.movieData.map((item, index) => {
+
+                return (
+                    <Row>
+
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={item.imade_url} />
+                                <Card.Body>
+                                    <Card.Title>{item.title}</Card.Title>
+                                    <Card.Text>
+                                        {item.overview}
+                                    </Card.Text>
+                                </Card.Body>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroupItem>{item.average_votes}</ListGroupItem>
+                                    <ListGroupItem>{item.total_votes}</ListGroupItem>
+                                    <ListGroupItem>{item.popularity}</ListGroupItem>
+                                    <ListGroupItem>{item.released_on}</ListGroupItem>
+                                </ListGroup>
+                                
+                            </Card>
+
+                        </Col>
+                    </Row>
+
+                )
+            })
+
+        )
+    }
+
+}
+
+export default Movie;
